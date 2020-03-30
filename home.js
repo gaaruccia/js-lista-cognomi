cc83708d7b63391152dd376db96f8e30b2dae643
Nicolò Battaglia <gaara0633@gmail.com>
2020-03-30 10:31:33 +0200

    fix

--- home.js
+++ home.js
@@ -1,7 +1,9 @@
 //Chiedi all’utente il cognome, inseriscilo in un array con altri cognomi e stampa la lista ordinata alfabeticamente.
+//Scrivi anche la posizione della lista in cui il nuovo utente si trova
 var cognomeUtente = prompt("Qual è il tuo congome?");
 var cognomeMaiuscolo = cognomeUtente.charAt(0).toUpperCase() + cognomeUtente.slice(1);
 var listaCognomi = ["Rizzati", "Orabona", "Fregni", "Molea", "DiBonaventura", "Ottaviani"];
 listaCognomi.push(cognomeMaiuscolo);
-listaCognomi.sort()
+listaCognomi.sort();
+console.log(listaCognomi.indexOf(cognomeMaiuscolo));
 document.getElementById('mio_id').innerHTML = listaCognomi.sort();
